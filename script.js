ef22dc61dc485af94ed2a6c3d37f1bd52fdadb2a
Rintaro Kudo <rinta.kudo@gmail.com>
2021-03-12 21:58:52 +0900

    beautified

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
-$(function() {
-	$('#pagepiling').pagepiling({
-	    menu: null,
-        direction: 'vertical',
-        verticalCentered: true,
-        sectionsColor: [],
-        anchors: [],
-        scrollingSpeed: 700,
-        easing: 'swing',
-        loopBottom: false,
-        loopTop: false,
-        css3: true,
-        navigation: {
-            'textColor': '#fff',
-            'bulletsColor': '#fff',
-            'position': 'right',
-            'tooltips': ['top', 'about', 'how to','game', 'next step']
-        },
-       	normalScrollElements: null,
-        normalScrollElementTouchThreshold: 5,
-        touchSensitivity: 5,
-        keyboardScrolling: true,
-        sectionSelector: '.section',
-        animateAnchor: false,
+$(function () {
+  $('#pagepiling').pagepiling({
+    menu: null,
+    direction: 'vertical',
+    verticalCentered: true,
+    sectionsColor: [],
+    anchors: [],
+    scrollingSpeed: 700,
+    easing: 'swing',
+    loopBottom: false,
+    loopTop: false,
+    css3: true,
+    navigation: {
+      'textColor': '#fff',
+      'bulletsColor': '#fff',
+      'position': 'right',
+      'tooltips': ['top', 'about', 'how to', 'game', 'next step']
+    },
+    normalScrollElements: null,
+    normalScrollElementTouchThreshold: 5,
+    touchSensitivity: 5,
+    keyboardScrolling: true,
+    sectionSelector: '.section',
+    animateAnchor: false,
 
-		//events
-		onLeave: function(index, nextIndex, direction){},
-		afterLoad: function(anchorLink, index){},
-		afterRender: function(){},
-	});
+    //events
+    onLeave: function (index, nextIndex, direction) {},
+    afterLoad: function (anchorLink, index) {},
+    afterRender: function () {},
+  });
 });
